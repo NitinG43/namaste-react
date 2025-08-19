35f027cfd5e4c49e8a3040f8701ed89e9db14315
Nitin Garg <gargnitin5234@gmail.com>
2025-08-19 16:02:55 +0530

    Episode 2

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [React.createElement(
-        "div",
-        {id:"child1"},
-        [React.createElement(
-            "h1",
-            {},
-            "I am h1 Tag"),
-        React.createElement(
-            "h2",
-            {},
-            "This is a h2 tag")
-    ]),
-    React.createElement(
-        "div",
-        {id:"child2"},[
-        React.createElement(
-            "h1",
-            {},
-            "This is h1 tag of 2nd child"),
-        React.createElement(
-            "h2",
-            {},
-            "This is the h2 tag of child 2")
-    ])
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child1" },
+      [
+        React.createElement("h1", {}, "I am h1 Tag"),
+        React.createElement("h2", {}, "This is a h2 tag")
+      ]
+    ),
+    React.createElement(
+      "div",
+      { id: "child2" },
+      [
+        React.createElement("h1", {}, "This is h1 tag of 2nd child"),
+        React.createElement("h2", {}, "This is the h2 tag of child 2")
+      ]
+    )
+  ]
+);
+
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
+
+
 
